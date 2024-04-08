7a8c2c4a34eaec2d8d342f549de51bebaee2b324
SubhaDas1 <subhadas20161@gmail.com>
2024-04-08 13:07:42 +0530

    all commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import {OtpComponent} from './otp/otp.component';
+import { OtpComponent} from './otp/otp.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,OtpComponent],
+  imports: [RouterOutlet,LoginComponent,OtpComponent,FooterComponent,HeaderComponent,RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
