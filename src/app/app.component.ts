4de8192151f6cd6570ec0ef7ff66a39e2e4408b0
arghadip123 <an.piku01@gmail.com>
2024-04-16 16:51:10 +0530

    commit on 16th

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { OtpComponent} from './otp/otp.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,OtpComponent,FooterComponent,HeaderComponent,RegistrationComponent],
+  imports: [RouterLink,RouterOutlet,LoginComponent,OtpComponent,FooterComponent,HeaderComponent,RegistrationComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { OtpComponent } from './otp/otp.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 export const routes: Routes = [
     {path:'', redirectTo: '', pathMatch: 'full' },
     {path:'login', component: LoginComponent},
-    {path:'otp', component: OtpComponent},
+    {path:'otp', component: OtpComponent},   
+    {path:'signin', component: RegistrationComponent}, 
 ];
